87f98cf76e1b3489518772536c246230c271518d
cristianku <c.zantedeschi@gmail.com>
2022-12-01 09:56:16 +0100

    c

--- turns_counter.ino
+++ turns_counter.ino
@@ -1,39 +1,53 @@
-#include<LiquidCrystal.h>
-LiquidCrystal lcd(12,11,6,5,4,3);
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); //need this text to work
 float value=0;
 float rev=0;
 int rpm;
 int oldtime=0;
 int time;
+float turns = 0;
 
-void isr() //interrupt service routine
-{
-rev++;
-}
+#define DIGITAL_PIN 2
+boolean sensorOut = HIGH;
+
+boolean previous_read = true;
+boolean display_done = false;
 
 void setup()
 {
 lcd.begin(16,2);                //initialize LCD
-pinMode(3,INPUT);
 Serial.begin(9600);
+pinMode(DIGITAL_PIN, INPUT);
+
 lcd.clear();
 lcd.setCursor(0,0);
-int turns = 0;
+// lcd.print("turns : 0");
 }
 
 void loop()
 {
-
-if (digitalRead(3)== LOW)
+sensorOut = digitalRead(DIGITAL_PIN);
+if (sensorOut == HIGH && previous_read ==false)
   {
     turns +=1;
+    previous_read = true;
+    display_done = false;
     
-    delay(1);
+    // delay(1);
 }
 
-if ( int (turns/100) == turns/100 ) {
+if (sensorOut == LOW)
+  {
+    previous_read = false;
+    
+    // delay(1);
+}
+
+if ( int (turns/10) == turns/10 && turns > 0 && display_done == false) {
     lcd.setCursor(0,1);
     lcd.print("turns :" + String(turns));
+    Serial.println(turns);
+    display_done = true;
 }
 
 }
